923b1de5245c0a18bb694b2f4b904ef8c2a0b022
jdecker77 <jdecker77@gmail.com>
2019-05-14 21:19:40 -0400

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-#!/Users/jessedecker/miniconda3/envs/gis/bin/python
-
-from Recipes import app
-import os
-
-app.secret_key = os.urandom(24)
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
